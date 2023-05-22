d1b45d855042e47f3c3ee1bb2605d38817c42d1e
KodingCore <131252253+KodingCore@users.noreply.github.com>
2023-05-22 09:37:05 +0200

    final du javaToolsWeb

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -1,79 +1,119 @@
-
-//import * as dayjs from 'dayjs'
-
-window.addEventListener('DOMContentLoaded', function(){
-    setInterval(callToolsListeners, 1);
+window.addEventListener('DOMContentLoaded', function () {
+    initialize();
+    setInterval(Update, 1);
 });
 
 
+let toggleXkey;
+let toggleCkey;
+let toggleVkey;
+let onePress;
 
-let isInit = false;
-let toggleXkey = true;
-let toggleCkey = true;
-let toggleVkey = true;
-let onePress = true;
+let elementsA;
 
-function callToolsListeners(){
-
-    if(!isInit){
-        
-        isInit = true;
-    }else if(isInit){
-        
-        $(document).keyup(function(event){
-            
-            if(event.key === "x" && onePress == true){
-                onePress = false;
-                overLineEmptyLinks(toggleXkey =! toggleXkey);
-            }
-            else if(event.key === "c" && onePress == true){
-                onePress = false;
-                columsDrawer(toggleCkey =! toggleCkey);
-            }
-            else if(event.key === "v" && onePress == true){
-                onePress = false;
-                viewportSizeViewer(toggleVkey =! toggleVkey);
-            }
-            
-        })
-        onePress = true;
-    }
-}
-
-function overLineEmptyLinks(boolTog){
-    if(boolTog){
-        
-    }else if(!boolTog){
-
-    }
-}
-
-function viewportSizeViewer(boolTog){
-    if(boolTog){
-
-    }else if(!boolTog){
-
-    }
-}
-
-let initColumnsDrawer = true;
 let overviewDraw;
 let columnNow;
 
-function columnsDrawer(boolTog) {
-  if (initColumnsDrawer) {
-    initColumnsDrawer = false;
-    overviewDraw = $("<div class='divOverDraw'></div>").appendTo("body");
+let overViewportSize;
+let textViewportSize;
 
+function initialize() {
+
+    //Keys Listener
+    toggleXkey = true;
+    toggleCkey = true;
+    toggleVkey = true;
+    onePress = true;
+
+    //Empty Links with #
+    elementsA = document.querySelectorAll('a');
+
+    //bootstrap overview
+    overviewDraw = $("<div class='divOverDraw'></div>").appendTo("body");
     for (let i = 0; i < 12; i++) {
         columnNow = $("<div class='divOverColumn'></div>").appendTo(overviewDraw);
-        columnNow.css({"left": i*100/12 + "%"});
-      }
-  }
+        columnNow.css("left", "0%");
+    }
 
-  if (boolTog) {
-    boolTog = !boolTog;
-  } else if (!boolTog) {
-    boolTog = !boolTog;
-  }
+    //Viewport Size
+    overViewportSize = $("<div class='divOverViewportsize'></div>").appendTo("body");
+    overViewportSize.css("display", "none");
+}
+
+function Update() {
+
+    overViewportSize.text("X= " + window.innerWidth + "px Y= " + window.innerHeight + "px");
+
+    //KEYS LISTENER
+            $(document).keypress(function (event) {
+                if (event.code === "Numpad1" && onePress == true) {
+                    onePress = false;
+                    overLineForUnlinked(toggleXkey = !toggleXkey);
+                }
+                else if (event.code === "Numpad2" && onePress == true) {
+                    onePress = false;
+                    columnsDrawer(toggleCkey = !toggleCkey);
+                }
+                else if (event.code === "Numpad3" && onePress == true) {
+                    onePress = false;
+                    viewportSizeViewer(toggleVkey = !toggleVkey);
+                }
+            });
+        
+    onePress = true;
+}
+
+
+/*            >>>>>>     UNLIKED OVERLINER     <<<<<<<        */
+//      THIS FUNCTION CREATE A LINE TROUGTH DE TEXT OF A LINK
+//          ONLY IF THIS LINK HREF ATTRIBUT SET AS #
+function overLineForUnlinked(boolTog) {
+    if (boolTog) {
+        boolTog = !boolTog;
+        elementsA.forEach(function (linker) {
+
+            //IF HREF ATTRIBUT OF A LINK AS THE SAME AS "#"
+            if (linker.getAttribute("href") == "#") {
+
+                //STYLE AN OVERLINE ON THE LINK
+                linker.style.textDecoration = "underline";
+                linker.style.color = "blue";
+            }
+        });
+    } else if (!boolTog) {
+        boolTog = !boolTog;
+        elementsA.forEach(function (linker) {
+
+            //IF HREF ATTRIBUT OF A LINK AS THE SAME AS "#"
+            if (linker.getAttribute("href") == "#") {
+
+                //STYLE AN OVERLINE ON THE LINK
+                linker.style.textDecoration = "line-through";
+                linker.style.color = "red";
+            }
+        });
+    }
+}
+
+
+function viewportSizeViewer(boolTog) {
+    if (boolTog) {
+        boolTog = !boolTog;
+        overViewportSize.css("display", "none");
+    } else if (!boolTog) {
+        boolTog = !boolTog;
+        overViewportSize.css("display", "flex");
+    }
+}
+
+
+
+function columnsDrawer(boolTog) {
+    if (boolTog) {
+        boolTog = !boolTog;
+        overviewDraw.css("display", "none");
+    } else if (!boolTog) {
+        boolTog = !boolTog;
+        overviewDraw.css("display", "flex");
+    }
 }
